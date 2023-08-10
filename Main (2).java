71ca9a9828638c77b048b975e4746b8aa98f1a13
Yash Rane <125262082+Yash-Rane@users.noreply.github.com>
2023-08-10 18:26:08 +0530

    Add files via upload
    non repeated string

---
+++ Main (2).java
@@ -0,0 +1,23 @@
+
+public class Main
+{
+	public static void main(String[] args) {
+	String input ="programming";
+	
+	for(int i =0;i<input.length();i++){
+	    boolean unique = true;
+	  for(int j=0;j<input.length();j++)  {
+	      if(i !=j && input.charAt(i)== input.charAt(j)){
+	          unique=false;
+	      }
+	      
+	  }
+	  if(unique){
+	      System.out.println(input.charAt(i));
+	      break;
+	  }
+	}
+	
+	
+	}
+}
